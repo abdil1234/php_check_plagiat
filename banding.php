98bbcf193d9cacb278e4f951a5e45bad78c47aa9
abdil lah <abdiillah98@gmail.com>
2019-01-22 02:00:09 +0700

    data

--- banding.php
+++ banding.php
@@ -21,11 +21,8 @@ if (!empty($_FILES['test'])) {
 	$old_file = dirname(__FILE__)."/important/banding/file_banding.txt";
     $upload->callbacks($validation, array('check_name_length'));
     
-	if(unlink($old_file)){
-        $results = $upload->upload('file_banding.txt');
-    }else{
-		die("Gagal hapus");
-	}
+    $results = $upload->upload('file_banding.txt');
+    
 	
     var_dump($results);
     echo "<a href='index.php'> Kembali</a>";
